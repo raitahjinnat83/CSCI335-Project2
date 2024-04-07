a2d5b2eca8cf5bbc68ea2b4eb72e99ebc23ca350
Raitah Jinnat <raitahjinnat@gmail.com>
2024-04-07 03:59:35 -0400

    heap header set up

---
+++ myHeap.hpp
@@ -0,0 +1,23 @@
+/**
+ * @file myHeap.hpp
+ * @author Raitah Jinnat
+ * @brief containing the prototypes* of the functions in myHeap.cpp
+ * @version 0.1
+ * @date 2024-04-07
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
+
+#ifndef MYHEAP_HPP
+#define MYHEAP_HPP
+
+#include <queue>
+#include <vector>
+#include <string>
+#include <iostream>
+#include <algorithm>
+
+void heapMedian(const std::vector<int>* instructions);
+
+#endif
