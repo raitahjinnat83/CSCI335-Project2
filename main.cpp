2277531c77e34cff3488e8c965f97da2b22e8c74
Raitah Jinnat <raitahjinnat@gmail.com>
2024-04-06 23:14:30 -0400

    created main file for testing

---
+++ main.cpp
@@ -0,0 +1,18 @@
+/**
+ * @file main.cpp
+ * @author Raitah Jinnat (raitah.jinnat83@myhunter.cuny.edu)
+ * @brief reads input from file, converts it to an integer, and passes it to each method
+ * @version 0.1
+ * @date 2024-04-06
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
+
+#include "myList.hpp"
+#include "myHeap.hpp"
+#include "myAVLtree.hpp"
+
+int main(){
+
+}
