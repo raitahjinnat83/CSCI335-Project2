c3817b1cf25fd266f9e52ead319d80484fb3ec1c
Raitah Jinnat <raitahjinnat@gmail.com>
2024-04-07 02:17:31 -0400

    list header created

---
+++ myList.hpp
@@ -0,0 +1,11 @@
+/**
+ * @file myList.hpp
+ * @author Raitah Jinnat
+ * @brief containing the prototypes* of the functions in myList.cpp
+ * @version 0.1
+ * @date 2024-04-07
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
+
