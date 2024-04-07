d08998b6aacb42d311b4f580d50f59f8e621ba10
Raitah Jinnat <raitahjinnat@gmail.com>
2024-04-07 04:17:51 -0400

    first draft of heap

--- myHeap.cpp
+++ myHeap.cpp
@@ -17,5 +17,28 @@ void heapMedian(const std::vector<int>* instructions) {
     std::priority_queue<int> maxHeap; // for elements <= median so the median is the top popped off
     std::priority_queue<int, std::vector<int>, std::greater<int>> minHeap; // for elements > median
 
+    for (int num : *instructions) {
+        if (num > -1) {
+            // Insert the number into the appropriate heap
+            if (maxHeap.empty() || num <= maxHeap.top()) {
+                maxHeap.push(num);
+            } else {
+                minHeap.push(num);
+            }
 
+            // Balance the heaps if necessary
+            if (maxHeap.size() > minHeap.size() + 1) {
+                minHeap.push(maxHeap.top());
+                maxHeap.pop();
+            } else if (minHeap.size() > maxHeap.size()) {
+                maxHeap.push(minHeap.top());
+                minHeap.pop();
+            }
+        } else {
+            // Pop the current median (which is the root of maxHeap)
+            int current_median = maxHeap.top();
+            std::cout << current_median << " "; // print it before popping
+            maxHeap.pop();
+        }
+    }
 }
