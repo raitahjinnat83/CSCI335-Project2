dd0f5ecf805f21513f37b3c03a60c30b82becdbf
Raitah Jinnat <raitahjinnat@gmail.com>
2024-04-08 22:57:56 -0400

    AVL implementation file created

---
+++ myAVLTree.hpp
@@ -0,0 +1,10 @@
+/**
+ * @file myAVLTree.hpp
+ * @author Raitah Jinnat
+ * @brief containing your implementation of an AVL tree data structure
+ * @version 0.1
+ * @date 2024-04-08
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
