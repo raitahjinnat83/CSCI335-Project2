25666c9209582f9334f2e8ad0af9f50a2b07286b
Raitah Jinnat <raitahjinnat@gmail.com>
2024-04-09 00:27:38 -0400

    AVL implementation created

---
+++ myAVLTree.cpp
@@ -0,0 +1,12 @@
+/**
+ * @file myAVLTree.cpp
+ * @author Raitah Jinnat
+ * @brief containing the function void treeMedian (const std::vector<int> * instructions)
+ * @version 0.1
+ * @date 2024-04-09
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
+
+#include "myAVLTree.hpp"
